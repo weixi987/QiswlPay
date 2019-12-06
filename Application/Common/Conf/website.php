840ed6397ae55f4cf2dc447d9c1e30d103590c21
dubox <dubox@duboxdeMacBook-Pro.local>
2019-12-07 07:44:53 +0800

    域名配置

--- Application/Common/Conf/website.php
+++ Application/Common/Conf/website.php
@@ -1,7 +1,7 @@
 <?php 
 		return array(
 			'WEB_TITLE' => '聚合支付',
-			'DOMAIN' => 'my.pay.com',
+			'DOMAIN' => $_SERVER['HTTP_HOST'],
 			'MODULE_ALLOW_LIST'   => array('Home','User','Admin','Install', 'Weixin','Pay','Cashier','Agent','Payment','Code'),
 			'URL_MODULE_MAP'  => array('admin'=>'admin', 'agent'=>'user', 'user'=>'user', 'code'=>'code'),
 			'LOGINNAME' => 'Login',
